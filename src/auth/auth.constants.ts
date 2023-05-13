7b170cc8b2b1bf81cf29c0a2015cf140764fe0e0
Mirgo25 <mirgo2513@gmail.com>
2023-05-13 14:21:09 +0300

    Added authorization guards

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'Such user has already been registered';
+export const USER_NOT_FOUND_ERROR = 'The user with such email is not found';
+export const WRONG_PASSWORD_ERROR = 'The user has the wrong password';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,26 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthDTO } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 
 @Controller('auth')
 export class AuthController {
-    
+    constructor(private readonly authService: AuthService) { }
+
+    @UsePipes(new ValidationPipe())
     @Post('register')
     async register(@Body() dto: AuthDTO) {
-
+        const oldUser = await this.authService.findUser(dto.login);
+        if (oldUser) {
+            throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+        }
+        return await this.authService.createUser(dto);
     }
 
     @HttpCode(200) // Handle StatusCode because POST return 201 by default, but Login does not change or create any data
     @Post('login')
-    async login(@Body() dto: AuthDTO) {
-
+    async login(@Body() { login, password }: AuthDTO) {
+        const { email } = await this.authService.validateUser(login, password);
+        return await this.authService.login(email);
     }
 }
