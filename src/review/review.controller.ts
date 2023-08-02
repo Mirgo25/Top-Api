11affa8a30c0e4ad675082f3a1fa6d3205a48264
Mirgo25 <mirgo2513@gmail.com>
2023-08-02 18:52:35 +0300

    realized telegram bot notification

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -17,15 +17,30 @@ import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { UserEmail } from '../decorators/user-email.decorator';
 import { IdValidationPipe } from '../pipes/id-validation.pipe';
+import { TelegramService } from 'src/telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-    constructor(private readonly reviewService: ReviewService) {}
+    constructor(
+        private readonly reviewService: ReviewService,
+        private readonly telegramService: TelegramService,
+    ) { }
 
     @UsePipes(new ValidationPipe())
     @Post('create')
     async create(@Body() dto: CreateReviewDTO) {
         return this.reviewService.create(dto);
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Post('notify')
+    async notify(@Body() dto: CreateReviewDTO) {
+        const message = `Name: ${dto.name}\n`
+            + `Title: ${dto.title}\n`
+            + `Description: ${dto.description}\n`
+            + `Rating: ${dto.rating}\n`
+            + `Product ID: ${dto.productId}`;
+        return this.telegramService.sendMessage(message);
     }
 
     @Get(':id')
